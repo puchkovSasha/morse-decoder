54e13dcfb8c86ffacce560c15c18d3a7cc1fe9b8
PuchkovSasha <Itworkpuchkov@yandex.ru>
2019-10-04 17:35:30 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -35,12 +35,53 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    
 };
 
+
 function decode(expr) {
-    // write your solution here
+
+    var reg = RegExp(/\d{10}|\*{10}/g);
+
+    var box = String(expr.match(reg));
+
+    var ch = box.replace(/11/g, '-');
+    var next = ch.replace(/10/g, '.');
+    var zero = next.replace(/0/g, '');
+
+    var MORSE = zero.replace(/([.-]+[-.]*)/g, (_, x) =>MORSE_TABLE [x]);
+    var spl = String(MORSE.replace(/,/g, ''));
+
+    var result = spl.replace(/\*{10}/g, ' ');
+   
+    return result;
+
 }
 
 module.exports = {
     decode
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
